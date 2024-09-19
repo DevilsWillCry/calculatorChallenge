04061e3ab20b99eceedbdf4fd67e750884a0930a
Miguel Angel <miguel.delta21@gmail.com>
2024-09-18 23:22:30 -0500

    Funcionalidad para tomar datos

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-const calculateButton = document.get
+const calculateButton = document.getElementById("button-calculator");
+const divCalculator = document.getElementsByClassName("calculator-container")[0];
+const divResults = document.getElementsByClassName("results-container")[0];
+
+calculateButton.addEventListener("click", () => {
+    const inputNumber1 = parseFloat((document.getElementById("bill")).value);
+    const inputNumber2 = parseFloat((document.getElementById("tip")).value);
+    divCalculator.classList.add("calculator-visibility");
+    divResults.classList.add("result-visibility");
+});
